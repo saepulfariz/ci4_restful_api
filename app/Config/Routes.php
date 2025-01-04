71991cf6d1673780ab2beb18b35c8a20b75c36c3
saepulfariz <saepulfariz108@gmail.com>
2025-01-04 14:19:17 +0700

    add auth filter token login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->resource('posts', ['controller' => 'Posts']);
 
 $routes->group("api", function ($routes) {
     $routes->post("register", "Register::index");
     $routes->post("login", "Login::index");
+
+    $routes->resource('posts', ['controller' => 'Posts']);
+    $routes->get("users", "Users::index", ['filter' => 'authFilter']);
 });
